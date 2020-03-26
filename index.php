82ebf2c320d84c6f7c58bd7f6b1e1cbd8b2a0993
yo <yo@DESKTOP-RVOAHLO>
2020-03-26 15:24:57 -0300

    initial commit.

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <title>Ejemplo_diurno</title>
+    </head>
+    <body>
+        <img src="https://media1.tenor.com/images/23894baf75e3d82e266695a97174da39/tenor.gif" />
+        <form action="" method="">
+            <input placeholder="Preguntele algo a ricardo milos 1313" type="text" />
+        </form>
+    </body>
+</html>
